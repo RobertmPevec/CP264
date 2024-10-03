37fa9d6f36d273c09750c8527eb2469df1d3e187
RobertmPevec <robertmpevec@gmail.com>
2024-10-03 15:41:57 -0400

    assignment 2 done

--- peve1145_a02/movie_utilities.c
+++ peve1145_a02/movie_utilities.c
@@ -12,16 +12,59 @@
 #include "movie_utilities.h"
 
 void get_movie(movie_struct *source) {
-    char movie_line[81];
-    printf("Enter a movie object (e.g. Star Wars: Episode IV - A New Hope|1977|George Lucas|8.7|0,6):\n");
-    fgets(movie_line, sizeof(movie_line), stdin);
+    char title[81];
+    int year;
+    char director[81];
+    int genre;
+    float rating;
+    printf("Title: ");
+    scanf(" %80[^\n]", title);
+    printf("Year: ");
+    scanf("%d", &year);
+    printf("Director: ");
+    scanf(" %80[^\n]", director);
+    printf("Genres\n"
+    "0: science fiction\n"
+    "1: fantasy\n"
+    "2: drama\n"
+    "3: romance\n"
+    "4: comedy\n"
+    "5: zombie\n"
+    "6: action\n"
+    "7: historical\n"
+    "8: horror\n"
+    "9: war\n"
+    "10: mystery\n"
+    "Genre: ");
+    scanf("%d", &genre);
+    if (genre < 0 || genre >= GENRES_COUNT - 1) {
+        genre = -1;
+    }
+    printf("Rating: ");
+    scanf("%f", &rating);
+    if (rating < 0.0 || rating > 10.0) {
+        rating = -1;
+    }
+    strcpy(source->title, title);
+    source->year = year;
+    strcpy(source->director, director);
+    source->genre = genre;
+    source->rating = rating;
+}
+
+void read_movie(movie_struct *source, const char *line) {
+    char movie_line[256];
+    strncpy(movie_line, line, sizeof(movie_line) - 1);
+    movie_line[sizeof(movie_line) - 1] = '\0';
     char *value = strtok(movie_line, "|");
     strcpy(source->title, value); 
     value = strtok(NULL, "|");
     char *pointer;
     source->year = strtol(value, &pointer, 10);
-}
-
-void read_movie(movie_struct *source, const char *line) {
-    
+    value = strtok(NULL, "|");
+    strcpy(source->director, value); 
+    value = strtok(NULL, "|");
+    source->rating = strtof(value, &pointer);
+    value = strtok(NULL, "|");
+    source->genre = strtol(value, &pointer, 10);
 }
